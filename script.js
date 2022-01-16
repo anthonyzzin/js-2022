91b5172b584b5bd15e97dfe665713ce8b5be0dc9
Anthony Almeida <80277056+anthonyzzin@users.noreply.github.com>
2022-01-16 18:44:00 -0300

    Primeiro desafio em JS de 2022

---
+++ script.js
@@ -0,0 +1,12 @@
+let name = "Anthony"
+let number = 19
+let year = 2022
+
+console.log(year - number)
+
+console.log("Estou mexendo no console.log")
+
+let quartaFeira = false
+
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
